c72187d29d6ecd3733e6608a657e4e577f915ca9
Jorge Santana <jorge@creacodigos.com>
2019-07-22 18:59:49 +0100

    Creando contenido de archivos

--- config/database.php
+++ config/database.php
@@ -1,13 +1,15 @@
 <?php
 class Database{
  
+    // especificar credenciales de conexión
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "api_db";
+    private $host     = "localhost";
+    private $db_name  = "api_db";
     private $username = "root";
     private $password = "";
-    public $conn;
+    public  $conn;
  
+    // obtener la conexión a la base de datos
     // get the database connection
     public function getConnection(){
  
@@ -17,7 +19,7 @@ class Database{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
+            echo "Error de Conexión: " . $exception->getMessage();
         }
  
         return $this->conn;
